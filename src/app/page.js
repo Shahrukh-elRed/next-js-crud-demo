9b212e85f25e9ac88130a9413a18770d9cbd8541
Shahrukh <shahrukh1494@gmail.com>
2024-03-18 10:51:40 +0530

    users page wqith dummy data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,51 @@
 export default function Home() {
+  const usersData = [
+    {
+      id: 1,
+      firstName: "John",
+      lastName: "Smith",
+      phoneNumber: "9810101010",
+      email: "john@test.com",
+    },
+    {
+      id: 2,
+      firstName: "Allen",
+      lastName: "Walker",
+      phoneNumber: "9810101234",
+      email: "allen@test.com",
+    },
+    {
+      id: 3,
+      firstName: "Mark",
+      lastName: "Manson",
+      phoneNumber: "9812345678",
+      email: "mark@test.com",
+    },
+  ];
+
   return (
     <main>
-      <h1>Home Page</h1>
+      <div className="user-table-header">Users List</div>
+      <table className="user-data-table">
+        <thead>
+          <tr>
+            <td>Sr. no.</td>
+            <td>Name</td>
+            <td>Phone Number</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {usersData.map((user, index) => (
+            <tr key={user.id}>
+              <td>{index + 1}</td>
+              <td>{user.firstName + " " + user.lastName}</td>
+              <td>{user.phoneNumber}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </main>
   );
 }
