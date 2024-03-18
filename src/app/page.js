92e4ec0a9cd26619524d72a5d8f41543ad8408a5
Shahrukh <shahrukh1494@gmail.com>
2024-03-18 11:46:25 +0530

    changed table and header css, made table responsive and added buttons

--- src/app/page.js
+++ src/app/page.js
@@ -49,43 +49,49 @@ export default function Home() {
           </span>
         </div>
       </div>
-      <table className="user-data-table">
-        <thead>
-          <tr>
-            <td>Sr. no.</td>
-            <td>Name</td>
-            <td>Phone Number</td>
-            <td>Email</td>
-            <td colSpan={2}>Actions</td>
-          </tr>
-        </thead>
-        <tbody>
-          {usersData.map((user, index) => (
-            <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{user.firstName + " " + user.lastName}</td>
-              <td>{user.phoneNumber}</td>
-              <td>{user.email}</td>
-              <td>
-                <button
-                  onClick={() => goToUserEdit(user.id)}
-                  className="user-table-delete-btn"
-                >
-                  Edit
-                </button>
-              </td>
-              <td>
-                <button
-                  onClick={() => deleteUser(user.id)}
-                  className="user-table-edit-btn"
-                >
-                  Delete
-                </button>
+      <div className="user-data-table-container">
+        <table className="user-data-table">
+          <thead>
+            <tr>
+              <td className="user-table-header-one">Sr. no.</td>
+              <td className="user-table-header-two">Name</td>
+              <td className="user-table-header-three">Phone Number</td>
+              <td className="user-table-header-four">Email</td>
+              <td className="user-table-header-five" colSpan={2}>
+                Actions
               </td>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {usersData.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td className="user-table-name">
+                  {user.firstName + " " + user.lastName}
+                </td>
+                <td>{user.phoneNumber}</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => goToUserEdit(user.id)}
+                    className="user-table-delete-btn"
+                  >
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => deleteUser(user.id)}
+                    className="user-table-edit-btn"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </main>
   );
 }
