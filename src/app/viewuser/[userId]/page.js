f1bc61047e75dab4cee159d991f0164a23cfebe4
Shahrukh <shahrukh1494@gmail.com>
2024-03-19 10:59:29 +0530

    single user get API and fetch single user

--- src/app/viewuser/[userId]/page.js
+++ src/app/viewuser/[userId]/page.js
@@ -1,41 +1,63 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
-const ViewUser = () => {
-  const userData = {
-    id: 1,
-    firstName: "John",
-    lastName: "Smith",
-    phoneNumber: "9810101010",
-    email: "john@test.com",
+const ViewUser = ({ params }) => {
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [apiError, setApiError] = useState(false);
+
+  const fetchUser = async () => {
+    let response = await fetch(`/api/users/${params.userId}`);
+    response = await response.json();
+    if (!response.success) setApiError(true);
+    setUserData(response.result);
+    setLoading(false);
   };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  // return <div className="single-user-not-found"></div>
 
   return (
     <div className="user-details-page-container">
       <div className="user-table-header margin-left-header">User Details</div>
-      <div className="user-detail-container">
-        <div className="user-detail-row">
-          <span className="user-detail-left-heading">User Id : </span>
-          <span className="user-detail-right">{userData._id}</span>
+      {!userData?.firstName && !loading && apiError ? (
+        <div className="loader-spinner-container">
+          <div>User not found</div>
         </div>
-        <div className="user-detail-row">
-          <span className="user-detail-left-heading">First Name : </span>
-          <span className="user-detail-right">{userData.firstName}</span>
+      ) : loading && !apiError ? (
+        <div className="loader-spinner-container">
+          <div className="loader-spinner"></div>
         </div>
-        <div className="user-detail-row">
-          <span className="user-detail-left-heading">Last Name : </span>
-          <span className="user-detail-right">
-            {userData.lastName ? userData.lastName : "N/A"}
-          </span>
+      ) : (
+        <div className="user-detail-container">
+          <div className="user-detail-row">
+            <span className="user-detail-left-heading">User Id : </span>
+            <span className="user-detail-right">{userData._id}</span>
+          </div>
+          <div className="user-detail-row">
+            <span className="user-detail-left-heading">First Name : </span>
+            <span className="user-detail-right">{userData.firstName}</span>
+          </div>
+          <div className="user-detail-row">
+            <span className="user-detail-left-heading">Last Name : </span>
+            <span className="user-detail-right">
+              {userData.lastName ? userData.lastName : "N/A"}
+            </span>
+          </div>
+          <div className="user-detail-row">
+            <span className="user-detail-left-heading">Phone Number : </span>
+            <span className="user-detail-right">{userData.phoneNumber}</span>
+          </div>
+          <div className="user-detail-row">
+            <span className="user-detail-left-heading">Email : </span>
+            <span className="user-detail-right">{userData.email}</span>
+          </div>
         </div>
-        <div className="user-detail-row">
-          <span className="user-detail-left-heading">Phone Number : </span>
-          <span className="user-detail-right">{userData.phoneNumber}</span>
-        </div>
-        <div className="user-detail-row">
-          <span className="user-detail-left-heading">Email : </span>
-          <span className="user-detail-right">{userData.email}</span>
-        </div>
-      </div>
+      )}
       <div className="home-link-container">
         <Link href="/" className="home-link">
           Go back to Home
